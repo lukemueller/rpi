f203ecacee38f22aca448101f1634a582fdf3667
Luke Mueller <luke.jordan.mueller@gmail.com>
2016-11-28 11:56:27 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## Live Templates
+
+`cp java.xml ~/Library/Preferences/IntelliJIdea2016.2/templates/`
+
+[IntelliJ Live Templates Help Page](https://www.jetbrains.com/help/idea/2016.2/sharing-live-templates.html)
---
+++ src/test/java/SetTest.java
@@ -0,0 +1,10 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+
+public class SetTest {
+}
